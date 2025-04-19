767d38faf4fb12571f6c72a53d59be4190ec46d4
RobloxFactoryRyaturmite <166668219+RobloxFactoryRyaturmite@users.noreply.github.com>
2025-04-19 01:57:04 -0400

    Update inf-jump.lua

--- inf-jump.lua
+++ inf-jump.lua
@@ -1,6 +1,3 @@
---[[
-	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
-]]
 local InfiniteJumpEnabled = true
 game:GetService("UserInputService").JumpRequest:connect(function()
 	if InfiniteJumpEnabled then
